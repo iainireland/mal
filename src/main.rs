8d5d8784ecfc8aca695ee0278223451f5a4abaef
Iain Ireland <iaini@ca.ibm.com>
2017-09-12 16:26:21 -0400

    Step 0

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,37 @@
+use std::io;
+use std::io::BufRead;
+use std::io::Write;
+
+type Expr = String;
+type Value = String;
+
+fn prompt() {
+  print!("user> ");
+  io::stdout().flush().unwrap();
+}
+
+fn read(input: &str) -> Expr {
+	input.to_string()
+}
+
+fn eval(expr: &Expr) -> Value {
+	expr.to_string()
+
+}
+
+fn print(val: &Value) -> String {
+	val.to_string()
+}
+
 fn main() {
-    println!("Hello, world!");
+	let stdin = io::stdin();
+   prompt();
+	for line in stdin.lock().lines() {
+	  let expr = read(&line.unwrap());
+	  let val = eval(&expr);
+	  let output = print(&val);
+	  println!("{}", output);
+     prompt();
+   }	
+  println!();
 }
